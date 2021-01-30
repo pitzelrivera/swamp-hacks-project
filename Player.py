fbc22df84c9c9c80f16c11376982e5fbf704c234
pitzelrivera <72771721+pitzelrivera@users.noreply.github.com>
2021-01-30 10:16:24 -0500

    Add files via upload

--- Player.py
+++ Player.py
@@ -1,4 +1,9 @@
 class Player:
+    name = ""
+    cards = list(0)
+    money = 0
+    get_out_of_jail_free_cards = 0
+
     def __init__(self, name, cards):
         self.name = name
         self.cards = list()
@@ -9,21 +14,16 @@ class Player:
         self.__money = input_Money
 
     def set_Cards(self, input_Cards):
-        self.__cards = cards.append(input_Cards)
+        self.__cards = self.cards.append(input_Cards)
 
     def set_Jail(self, numJailFree):
-        get_out_of_jail_free_cards + numJailFree
+        self.get_out_of_jail_free_cards + numJailFree
 
     def get_Name(self):
-        return name
+        return self.name
 
     def get_Money(self):
-        return money
+        return self.money
 
     def get_Jail(self):
-        return get_out_of_jail_free_cards
-
-    
-
-
-
+        return self.get_out_of_jail_free_cards
