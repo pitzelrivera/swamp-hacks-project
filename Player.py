b587cdb2cd63d61087743996752a64943a146b0e
fostersarah <73560775+fostersarah@users.noreply.github.com>
2021-01-30 07:46:36 -0500

    Add files via upload

---
+++ Player.py
@@ -0,0 +1,20 @@
+class Player:
+    def __init__(self, name, cards, money):
+        self.name = name
+        self.cards = list()
+        self.money = money
+    
+    def set_Name(self, input_Name):
+        self.__name = input_Name
+
+    def set_Money(self, input_Money):
+        self.__money = input_Money
+
+    def set_Cards(self, input_Cards):
+        self.__cards = cards.append(input_Cards)
+
+    def get_Name(self):
+        return name
+
+    def get_Money(self):
+        return money
